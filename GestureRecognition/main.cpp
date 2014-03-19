edd54222f1168ccef81966173292736c8dae341f
Y. T. Chung <zonyitoo@gmail.com>
2014-03-19 17:09:59 +0800

    Initial Commit

---
+++ GestureRecognition/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GestureRecognition
+//
+//  Created by ELTON on 19/3/14.
+//  Copyright (c) 2014年 钟宇腾. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
